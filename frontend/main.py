3f0d5261ef80600e701c32de155812a1ba01142e
Đinh Cao Minh Quân <minhquandinhcao@gmail.com>
2024-12-19 16:36:20 +0700

    fix(main): Fix page navigation.

--- frontend/main.py
+++ frontend/main.py
@@ -10,7 +10,6 @@ def get_response(endpoint, params=None):
         return response.json()
     except:
         st.error("Failed to connect to the API.")
-        return None
 
 def get_all_genres():
     response = get_response("movies/genres")
@@ -26,6 +25,8 @@ def get_suggestions(query):
     if not response or response[0].lower() != query.strip().lower():
         response.insert(0, query.strip())
 
+    response = [response[0]] + [response[i] for i in range(1, len(response)) if response[i] != response[i - 1]]
+
     return response
 
 def search_movies():
@@ -40,8 +41,8 @@ def search_movies():
         params["query"] = query
     if genres is not None and len(genres) > 0:
         params["genres"] = genres
-    if release_year is not None and release_year.strip():
-        params["release_year"] = release_year
+    if release_year is not None and release_year != (1900, 2030):
+        params["release_year"] = release_year[0]
     if director is not None and director.strip():
         params["director"] = director
     if casts is not None and casts.strip():
@@ -50,22 +51,30 @@ def search_movies():
     response = get_response("movies/search", params)
     return (response["total"], response["results"])
 
+def load_more():
+    st.session_state.page += 1
+    st.session_state.results[1].extend(search_movies()[1])
+
 def run():
     st.set_page_config(page_title="Movie Text Search", layout="wide")
     st.markdown("<h1 style='text-align: center;'>Movie Text Search</h1>", unsafe_allow_html=True)
 
     st.sidebar.title("Advanced Search Options")
-    genres = st.sidebar.multiselect("Genres", get_all_genres())
-    release_year = st.sidebar.text_input("Release Year", placeholder="Enter release year")
-    director = st.sidebar.text_input("Director", placeholder="Enter director name")
-    casts = st.sidebar.text_input("Casts", placeholder="Enter cast names, separated by commas")
+    genres = st.sidebar.multiselect("Genres", get_all_genres(), key="id_genres")
+    release_year = st.sidebar.slider("Release Year", 1900, 2030, (1900, 2030), key="id_release_year")
+    director = st.sidebar.text_input("Director", placeholder="Enter director name", key="id_director")
+    casts = st.sidebar.text_input("Casts", placeholder="Enter cast names, separated by commas", key="id_casts")
     
+    last_query = st.session_state.query if "query" in st.session_state else ""
     query = st_searchbox(get_suggestions, "Search text for the movie (e.g., title, plot)")
 
     _, col, _ = st.columns([1, 1, 1])
     submit = col.button("Search", use_container_width=True, type="primary")
+
+    if query is None:
+        query = ""
     
-    if submit or query:
+    if submit or (query and query != last_query):
         st.session_state.query = query
         st.session_state.genres = genres
         st.session_state.release_year = release_year
@@ -78,45 +87,32 @@ def run():
         return
 
     if st.session_state.results[0] == 0:
-        st.warning("No results found.")
+        st.warning(f"No results found for '{st.session_state.query}'.")
         return
 
-    st.success(f"Found {st.session_state.results[0]} results.")
-    for result in st.session_state.results[1]:
-        with st.expander(f"**{result['title']} ({result['release_date'][:4]}) | Average Rating: {result['vote_average']}**", expanded=True):
-            if result["homepage"] != "Unknown":
-                st.markdown(f"## [{result['title']} ({result['release_date'][:4]})]({result['homepage']})")
-            else:
-                st.markdown(f"## {result['title']} ({result['release_date'][:4]})", unsafe_allow_html=True)
-            st.markdown(f"#### Directed by: {result['director']}", unsafe_allow_html=True)
-            st.markdown(f"**Genres:** {', '.join(result['genres'])} | **Release Date:** {result['release_date']} | **Runtime:** {result['runtime']} minutes | **Popularity:** {result['popularity']}")
-            st.markdown(f"{result['overview']}")
+    st.success(f"Found {st.session_state.results[0]} results for '{st.session_state.query}'.")
 
-    is_first_page = st.session_state.page == 1
-    is_last_page = st.session_state.results[0] <= st.session_state.page * 10
+    for i, result in enumerate(st.session_state.results[1]):
+        with st.expander(f"**Result #{i + 1}: {result['title']} ({result['release_date'][:4]}) | Average Rating: {float(result['vote_average']):.2f}**", expanded=True):
+            st.markdown(f"## {result['title']} ({result['release_date'][:4]})", unsafe_allow_html=True)
+            st.markdown(f"#### Directed by: {', '.join(result['director'])}", unsafe_allow_html=True)
+            st.markdown(f"**Genres:** {', '.join(result['genres'])} | **Release Date:** {result['release_date']} | **Runtime:** {result['runtime']} minutes | **Popularity:** {float(result['popularity']):.2f}")
 
-    if is_first_page and is_last_page:
+            if "overview" in result:
+                overview = result["overview"]
+            elif "plot_synopsis" in result:
+                overview = result["plot_synopsis"]
+            overview = str(overview).strip()
+            if len(overview) > 500:
+                overview = overview[:500] + "..."
+
+            st.markdown(f"{overview}")
+
+    if st.session_state.results[0] <= st.session_state.page * 10:
         return
 
-    _, col1, col2 , col3, _ = st.columns([11, 5, 1, 5, 11])
-    col2.markdown(f"<p style='text-align: center; padding-top: 5px;'>{st.session_state.page}</p>", unsafe_allow_html=True)
-    if is_first_page:
-        prev_page = col1.button("Previous page", use_container_width=True, type="primary", disabled=True)
-    else:
-        prev_page = col1.button("Previous page", use_container_width=True, type="primary")
-
-    if is_last_page:
-        next_page = col3.button("Next page", use_container_width=True, type="primary", disabled=True)
-    else:
-        next_page = col3.button("Next page", use_container_width=True, type="primary")
-    
-    if prev_page is not None and prev_page:
-        st.session_state.page -= 1
-        st.session_state.results = search_movies()
-
-    if next_page is not None and next_page:
-        st.session_state.page += 1
-        st.session_state.results = search_movies()
+    _, col, _ = st.columns([2, 1, 2])
+    col.button("Load more...", use_container_width=True, on_click=load_more)
 
 if __name__ == "__main__":
     run()
